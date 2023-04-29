1f863fdba50cf2efff921c2cc4363049aa256753
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2023-04-29 16:55:17 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# CFF Gateway Charges
+
+This module adds Gateway Charges features to CHIP for Fluent Forms. It requires installation of FluentForms plugin and CHIP for Fluent Forms plugin.
+
+This plugin utilize below filter to achieve the gateway charges:
+
+- ff_chip_create_purchase_params
+- ff_chip_handle_paid_data
+
+## Installation
+
+* [Download zip file of CFF Gateway Charges plugin.](https://github.com/CHIPAsia/cff-gateway-charges/archive/refs/heads/main.zip)
+* Log in to your Wordpress admin panel and go: **Plugins** -> **Add New**
+* Select **Upload Plugin**, choose zip file you downloaded in step 1 and press **Install Now**
+* Activate plugin
+
+## Other
+
+Facebook: [Merchants & DEV Community](https://www.facebook.com/groups/3210496372558088)
---
+++ chip-fluent-forms-gateway-charges.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *
+ * Plugin Name: CHIP FluentForms Gateway Charges
+ * Description: This to inject gateway charges to total amount
+ * Version: 1.0.0
+ */
+
+add_filter( 'ff_chip_create_purchase_params', 'cff_inject_gateway_charges', 10, 4 );
+// add_filter( 'ff_chip_handle_paid_data', 'cff_reverse_total_amount', 10, 4 );
+
+function cff_minimum_fee() {
+  return 150; // RM 1.50
+}
+
+function cff_variable_rate() {
+  return 0.022; // 2.2%
+}
+
+function cff_fixed_rate() {
+  return 100; // RM 1.00
+}
+
+function cff_product_title() {
+  return 'Processing Fee';
+}
+
+function cff_inject_gateway_charges( $params, $transaction, $submission, $form ) {
+
+  $calculated_fee = round( $params['purchase']['products'][0]['price'] * cff_variable_rate() + cff_fixed_rate() );
+
+  if ( $calculated_fee < cff_minimum_fee() ) {
+    $calculated_fee = cff_minimum_fee();
+  }
+
+  $params['purchase']['products'][] = array(
+    'name'     => cff_product_title(),
+    'price'    => $calculated_fee,
+  );
+
+  return $params;
+}
+
+function cff_reverse_total_amount( $transaction_data, $submission, $transaction, $vendorTransaction ) {
+  foreach( $vendorTransaction['purchase']['products'] as $product ) {
+    if ( $product['name'] != cff_product_title() ) {
+      $transaction_data['payment_total'] = intval( $product['price'] );
+      break;
+    }
+  }
+  
+  return $transaction_data;
+}
